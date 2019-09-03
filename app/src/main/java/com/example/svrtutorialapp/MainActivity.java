aca8c3b281a547b20d1a8ef692e70fd0a407e419
Afonso R. Costa Jr <arabelo@gmail.com>
2019-09-03 10:22:58 -0400

    Check camera permission on the first usage

--- app/src/main/java/com/example/svrtutorialapp/MainActivity.java
+++ app/src/main/java/com/example/svrtutorialapp/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.svrtutorialapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import com.samsungxr.SXRActivity;
 
-public class MainActivity extends SXRActivity {
+public class MainActivity extends SXRActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setMain(new Main());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            setMain(new Main());
+        } else {
+            requestCameraPermission();
+        }
+
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setMain(new Main());
+            } else {
+                Toast.makeText(getApplicationContext(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
